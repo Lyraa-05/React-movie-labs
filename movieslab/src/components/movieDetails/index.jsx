077bee73421bbc955dcd29b0236d95c5b21742fd
Lyraa-05 <20106693@mail.wit.ie>
2025-10-14 14:23:02 +0100

    Exercise movie details page - added production countries.

--- movieslab/src/components/movieDetails/index.jsx
+++ movieslab/src/components/movieDetails/index.jsx
@@ -59,6 +59,16 @@ const MovieDetails = ({ movie }) => {
         />
         <Chip label={`Released: ${movie.release_date}`} />
       </Paper>
+      <Paper component="ul" sx ={{...root}}>
+        <li>
+        <Chip label="Countries" sx={{...chip}} color="primary"/>
+        </li>
+        {movie.production_countries.map((c) => (
+            <li key= {c.name}>
+                <Chip label={c.name} sx={{...chip}} />
+            </li>
+        ))}
+      </Paper>
         <Fab
         color="secondary"
         variant="extended"
